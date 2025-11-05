fc1c2de0d719226fcd74b6309f28e29820715837
Cidospark <cidospark27@gmail.com>
2025-11-05 05:54:32 +0100

    DbContext, Identity and Infrastructure layer config added

---
+++ FraudGuard/FraudGuard.Infrastructure/Data/FraudDbContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FraudGuad.Domain.Entities;
+using FraudGuard.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FraudGuard.Infrastructure.Data
+{
+    public class FraudDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public FraudDbContext(DbContextOptions<FraudDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Alert> Alerts { get; set; }
+        public DbSet<Case> Cases { get; set; }
+        public DbSet<CaseAlerts> CaseAlerts { get; set; }
+        public DbSet<Rule> Rules { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+    }
+}
---
+++ FraudGuard/FraudGuard.Infrastructure/InfrastructureServicesConfig.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FraudGuard.Infrastructure.Data;
+using FraudGuard.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FraudGuard.Infrastructure
+{
+    public static class InfrastructureServicesConfig
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<FraudDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            // services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            // {
+            //     options.Password.RequiredLength = 8;
+            //     options.Password.RequireNonAlphanumeric = false;
+            // })
+            // .AddEntityFrameworkStores<FraudDbContext>()
+            // .AddDefaultTokenProviders();
+
+            return services;
+        }
+
+    }
+}
